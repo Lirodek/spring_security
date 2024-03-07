477c2977c10e1bf83b96d7f7232249f73f5a97da
Lirodek <96456897+Lirodek@users.noreply.github.com>
2024-03-08 08:09:10 +0900

    update

--- springSecurityTest/demo-security/src/test/java/com/security/demosecurity/account/AccountTest.java
+++ springSecurityTest/demo-security/src/test/java/com/security/demosecurity/account/AccountTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 
@@ -23,8 +26,24 @@ public class AccountTest {
     MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
     public void index_anonymous() throws Exception {
-        mockMvc.perform(get("/admin").with(user("keesun").roles("USER")))
+        mockMvc.perform(get("/admin"))
+            .andDo(print())
+            .andExpect(status().is(401));
+    }
+
+    @Test
+    @WithMockUser(username = "keesun", roles = "USER")
+    public void index_user() throws Exception {
+        mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_admin() throws Exception {
+        mockMvc.perform(get("/").with(user("admin").roles("ADMIN")))
             .andDo(print())
             .andExpect(status().isOk());
     }
